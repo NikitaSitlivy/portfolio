7382c25d9ed35406ddd191767022570c10426f90
Nikita <nikita.sitlivy@mail.ru>
2026-02-06 15:54:37 +0300

    front(feat): выкатка для хоста

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  // GitHub Pages project site base path
+  base: "/portfolio/",
+  build: {
+    outDir: "docs"
+  },
   plugins: [vue()]
 });
